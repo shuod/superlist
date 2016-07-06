2e5d9f7f67a9c991f36c3cf28955166740a41f9b
shuod <shuo.dai@okstate.edu>
2016-07-06 16:52:57 -0500

    Setup a basic HTML(title) and resovle root url to it. unit test for this.

--- lists/views.py
+++ lists/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.shortcuts import render
 
 # Create your views here.
-def home_page():
-    pass
+
+from django.http import HttpResponse
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
 

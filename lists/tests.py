17e9decfd311067c0c6fcb81de1c9023e5be8553
shuod <shuo.dai@okstate.edu>
2016-07-06 17:36:39 -0500

    add functional test to a response in HTML containing a table.

--- lists/tests.py
+++ lists/tests.py
@@ -10,6 +10,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
 
@@ -24,3 +25,9 @@ class HomePageTest(TestCase):
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
 
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
